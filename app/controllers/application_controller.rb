1f3fae869139bb8a9bb63fc15c7b4f6b2a78b0a1
Андрей Мельник <andrew.melnyk55@gmail.com>
2016-08-21 01:04:06 +0300

    rendering results in json added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,31 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  respond_to :json, :html
 
   def index
   end
+
+  def analyze
+    input = params[:dataset].split(',').map { |x| x.strip.to_i }
+    output = {}
+    output[:min] = input.min
+    output[:max] = input.max
+    output[:average] = input.reduce(:+) * 1.0 / input.size
+    output[:q1] = input.max
+    output[:q3] = input.max
+    output[:median] = input.max
+    output[:outliers] = input.max
+    render json: {
+        average: output[:average],
+        min: output[:min],
+        max: output[:max],
+        q1: output[:q1],
+        q3: output[:q3],
+        median: output[:median],
+        outliers: output[:outliers]
+    }
+  end
+
 
 end
