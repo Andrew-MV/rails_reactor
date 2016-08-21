cc6358cdd8de07598f4da851443334136b752570
Андрей Мельник <andrew.melnyk55@gmail.com>
2016-08-21 15:40:59 +0300

    created authorization controller

--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,8 @@ ActiveRecord::Schema.define(version: 20160820185304) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
+    t.string   "login"
+    t.string   "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
