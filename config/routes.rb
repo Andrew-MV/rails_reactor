87ada04efe5a98756ba1e53bde30192719ac0a7d
Андрей Мельник <andrew.melnyk55@gmail.com>
2016-08-21 18:41:13 +0300

    routes updated for authorization

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   scope 'api' do
     post '/analyze', to: 'application#analyze'
     post '/correlate', to: 'application#correlate'
+    post '/sign-up', to: 'authorization#sign_up'
+    post '/sign-in', to: 'authorization#sign_in'
   end
 
   root 'application#index'
