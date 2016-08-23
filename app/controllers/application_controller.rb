7cde6dd25be4d4b50acdfc60caedd492e11ae1b4
Андрей Мельник <andrew.melnyk55@gmail.com>
2016-08-23 13:00:29 +0300

    results calculation added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,11 +20,11 @@ class ApplicationController < ActionController::Base
       output = {}
       output[:min] = input.min
       output[:max] = input.max
-      output[:average] = input.max
-      output[:q1] = input.max
-      output[:q3] = input.max
-      output[:median] = input.max
-      output[:outliers] = input.max
+      output[:average] = find_average(input).round(3)
+      output[:q1] = find_q1(input)
+      output[:q3] = find_q3(input)
+      output[:median] = find_median(input)
+      output[:outliers] = find_outliers(input)
       render json: output
     end
   end
@@ -53,7 +53,7 @@ class ApplicationController < ActionController::Base
         }, status: 422
       else
         output = {}
-        output[:answer] = 'correlation result'
+        output[:answer] = find_correlation(input1, input2).round(3)
         render json: output
       end
     end
@@ -79,6 +79,47 @@ class ApplicationController < ActionController::Base
       end
     end
     error
+  end
+
+  def find_average(array)
+    array.reduce(:+).fdiv(array.size)
+  end
+
+  def find_median(array)
+    index = array.size - 1
+    array.sort[(index / 2)..(index - (index / 2))].reduce(:+).fdiv(2 ** (index % 2))
+  end
+
+  def find_q1(array)
+    find_median(array.sort[0..(array.size / 2 - 1)])
+  end
+
+  def find_q3(array)
+    index = array.size + 1
+    find_median(array.sort[(index / 2)..-1])
+  end
+
+  def find_outliers(array)
+    outliers = []
+    diff = find_q3(array) - find_q1(array)
+    left_border = find_q1(array) - 1.5 * diff
+    right_border = find_q3(array) + 1.5 * diff
+    array.each do |num|
+      outliers << num unless (left_border..right_border) === num
+    end
+    outliers.empty? ? 'None' : outliers.sort.join('; ')
+  end
+
+  def find_correlation(array1, array2)
+    arr1_avg = find_average(array1)
+    arr2_avg = find_average(array2)
+    numerator = arr1_sum = arr2_sum = 0
+    array1.size.times do |index|
+      numerator += (array1[index] - arr1_avg) * (array2[index] - arr2_avg)
+      arr1_sum += (array1[index] - arr1_avg) ** 2
+      arr2_sum += (array2[index] - arr2_avg) ** 2
+    end
+    numerator.fdiv((arr1_sum * arr2_sum) ** 0.5)
   end
 
 end
