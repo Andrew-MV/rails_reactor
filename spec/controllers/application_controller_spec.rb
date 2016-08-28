7c42264dd3b18aaaf440ddb3e90489c949d44fc1
Андрей Мельник <andrew.melnyk55@gmail.com>
2016-08-28 15:12:26 +0300

    specs for controllers added

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -2,4 +2,89 @@ require 'rails_helper'
 
 RSpec.describe ApplicationController, type: :controller do
 
+  context 'response of #analyze should' do
+
+    def params(dataset)
+      {
+          meta: {
+              dataset: dataset
+          }
+      }
+    end
+
+    it 'fail due to empty dataset' do
+      post :analyze, params(nil), format: :json
+      expect(response).to be_bad_request
+    end
+
+    it 'fail due to invalid dataset' do
+      post :analyze, params(''), format: :json
+      expect(response).to be_bad_request
+    end
+
+    it 'fail due to invalid dataset' do
+      post :analyze, params('1,2,g'), format: :json
+      expect(response).to be_bad_request
+    end
+
+    context 'be successful due to valid dataset' do
+      dataset1 = '  1 , 2 ,3, 4, 5, '
+      dataset2 = '1,1,1,11,11,11,11,11,11,11,11,11,11,11'
+      it 'and return correct q1' do
+        post :analyze, params(dataset1), format: :json
+        parsed_response = JSON.parse(response.body)
+        expect(parsed_response['meta']['q1']).to eq 1.5
+      end
+      it 'and return correct outliers' do
+        post :analyze, params(dataset2), format: :json
+        parsed_response = JSON.parse(response.body)
+        expect(parsed_response['meta']['outliers']).to eq '1; 1; 1'
+      end
+    end
+
+  end
+
+  context 'response of #correlate should' do
+
+    def params(dataset1, dataset2)
+      {
+          meta: {
+              dataset1: dataset1,
+              dataset2: dataset2
+          }
+      }
+    end
+
+    it 'fail due to empty dataset' do
+      post :correlate, params('2,3,4', nil), format: :json
+      expect(response).to be_bad_request
+    end
+
+    it 'fail due to invalid dataset' do
+      post :correlate, params('1,3,g', ' 5 ,6, 5 '), format: :json
+      expect(response).to be_bad_request
+    end
+
+    it 'fail due to invalid dataset' do
+      post :correlate, params('1,1,1', '2,3,4'), format: :json
+      expect(response).to be_bad_request
+    end
+
+    it 'fail due to different size of arrays' do
+      post :correlate, params('1,1,1,5', '2,3,4'), format: :json
+      expect(response).to be_bad_request
+    end
+
+    context 'be successful due to valid datasets' do
+      dataset1 = ['  1 , 2 ,3, 4, 5, ']
+      dataset2 = ['1,2,3,7,11']
+      it 'and return correct value' do
+        post :correlate, params(dataset1[0], dataset2[0]), format: :json
+        parsed_response = JSON.parse(response.body)
+        expect(parsed_response['meta']['answer']).to be_within(1).of(0)
+      end
+    end
+
+  end
+
 end
--- spec/controllers/authorization_controller_spec.rb
+++ spec/controllers/authorization_controller_spec.rb
@@ -2,4 +2,37 @@ require 'rails_helper'
 
 RSpec.describe AuthorizationController, type: :controller do
 
+  let(:user_1) { User.new(login: 'user1', password: 'user1'    ) }
+  let(:user_2) { User.new(login: 'user1', password: 'different') }
+  let(:user_3) { User.new(login:      '', password: 'user1'    ) }
+  let(:user_4) { User.new(login: 'userN', password: 'different') }
+
+  def params(user)
+    {
+        meta: {
+            login:    user.login,
+            password: user.password
+        }
+    }
+  end
+
+  context 'sign up' do
+    it 'should be successful' do
+      post :sign_up, params(user_1), format: :json
+      expect(response).to be_success
+    end
+
+  end
+
+  context 'sign in' do
+
+    context 'should fail for' do
+      it 'user with empty login' do
+        post :sign_up, params(user_3), format: :json
+        expect(response).to be_bad_request
+      end
+
+    end
+  end
+
 end
